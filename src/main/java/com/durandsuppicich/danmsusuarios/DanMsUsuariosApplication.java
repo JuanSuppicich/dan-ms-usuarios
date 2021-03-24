913608da900dcd27f27d8039046ece1b334f37d6
JuanSuppicich <suppicichjuan@gmail.com>
2021-03-24 16:16:14 -0300

    Initial commit

---
+++ src/main/java/com/durandsuppicich/danmsusuarios/DanMsUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.durandsuppicich.danmsusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanMsUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanMsUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/durandsuppicich/danmsusuarios/domain/Empleado.java
@@ -0,0 +1,5 @@
+package com.durandsuppicich.danmsusuarios.domain;
+
+public class Empleado {
+    
+}
