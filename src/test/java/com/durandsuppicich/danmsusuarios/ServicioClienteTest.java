f9128e0a1b1ab14b795c87cd285538ebedc422b0
JuanSuppicich <suppicichjuan@gmail.com>
2021-04-24 17:04:56 -0300

    Try to solve jenkins problem

--- src/test/java/com/durandsuppicich/danmsusuarios/ServicioClienteTest.java
+++ src/test/java/com/durandsuppicich/danmsusuarios/ServicioClienteTest.java
@@ -29,7 +29,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest
+@SpringBootTest(classes = DanMsUsuariosApplication.class)
 public class ServicioClienteTest {
 
     @Autowired
